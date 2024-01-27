b26b3f3fc7fda5e50ece3513c2597375a5631245
maranix <ramanverma4183@gmail.com>
2024-01-28 00:18:39 +0530

    docs (rust/generator): be more specific about empty cli arguments

--- rust/src/generator.rs
+++ rust/src/generator.rs
@@ -7,13 +7,15 @@ fn main() {
         .nth(1)
         .unwrap_or_else(|| {
             eprintln!("Expected some number to be passed as an argument.");
+            eprintln!("Consider the below example for generating 10000 rows.\n");
+            eprintln!("cargo run --bin generator -- 10000");
             process::exit(1);
         })
         .parse::<i32>()
         .unwrap_or_else(|_| {
-            println!("Could not parse the argument as i32.");
-            println!("Make sure a valid integer was provided as argument.");
-            println!("Falling back to default: 1_000_000_000");
+            eprintln!("Could not parse the argument as i32.");
+            eprintln!("Make sure a valid integer was provided as argument.");
+            eprintln!("Falling back to default: 1_000_000_000");
             DEFAULT_ROW_COUNT
         });
 
