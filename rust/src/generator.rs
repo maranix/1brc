04adceb4727fe59377ebbf648be6bd375d6c4a7d
maranix <ramanverma4183@gmail.com>
2024-01-28 00:16:21 +0530

    feat (rust/generator): parse row count argument.

--- rust/src/generator.rs
+++ rust/src/generator.rs
@@ -1,3 +1,21 @@
+use std::{env, process};
+
+const DEFAULT_ROW_COUNT: i32 = 1_000_000_000;
+
 fn main() {
-    println!("Generator");
+    let rows = &env::args()
+        .nth(1)
+        .unwrap_or_else(|| {
+            eprintln!("Expected some number to be passed as an argument.");
+            process::exit(1);
+        })
+        .parse::<i32>()
+        .unwrap_or_else(|_| {
+            println!("Could not parse the argument as i32.");
+            println!("Make sure a valid integer was provided as argument.");
+            println!("Falling back to default: 1_000_000_000");
+            DEFAULT_ROW_COUNT
+        });
+
+    println!("{}", &rows);
 }
